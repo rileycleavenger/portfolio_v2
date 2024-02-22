a32c1ab2b1844cc572b6f560a7c4bc542ed82e28
rileycleavenger <riley.cleavenger@ufl.edu>
2024-02-22 15:25:46 -0500

    icons are scrolling

--- riley-portfolio/src/components/about/about.tsx
+++ riley-portfolio/src/components/about/about.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import './about.css';
-import { FaLinkedin, FaGithub } from 'react-icons/fa';
+import { FaJsSquare, FaPython, FaReact, FaAngular, FaHtml5, FaCss3Alt, FaGithub, FaJava, FaNodeJs, FaLinkedin } from 'react-icons/fa';
+import { DiJavascript1, DiFirebase, DiRust, DiVisualstudio } from 'react-icons/di';
+import { SiCplusplus, SiCsharp, SiMysql, SiExpress, SiFlask, SiVercel, SiMicrosoftazure, SiRaspberrypi, SiJirasoftware } from 'react-icons/si';
+import { BiLogoTypescript, BiLogoGoLang  } from "react-icons/bi";
 
 const About: React.FC = () => {
   return (
@@ -22,6 +25,34 @@ const About: React.FC = () => {
             </a>
           </div>
         </div>
+      </div>
+      <div className="icons-container">
+      <div className="scrolling-icons">
+        <FaJsSquare className="moving-icon"/>
+        <FaPython className="moving-icon"/>
+        <FaReact className="moving-icon"/>
+        <FaAngular className="moving-icon"/>
+        <FaHtml5 className="moving-icon"/>
+        <FaCss3Alt className="moving-icon"/>
+        <FaGithub className="moving-icon"/>
+        <FaJava className="moving-icon"/>
+        <FaNodeJs className="moving-icon"/>
+        <DiJavascript1 className="moving-icon"/>
+        <BiLogoTypescript className="moving-icon"/>
+        <DiFirebase className="moving-icon"/>
+        <DiRust className="moving-icon"/>
+        <DiVisualstudio className="moving-icon"/>
+        <SiMicrosoftazure  className="moving-icon"/>
+        <SiCplusplus className="moving-icon"/>
+        <SiCsharp className="moving-icon"/>
+        <BiLogoGoLang className="moving-icon"/>
+        <SiMysql className="moving-icon"/>
+        <SiExpress className="moving-icon"/>
+        <SiFlask className="moving-icon"/>
+        <SiVercel className="moving-icon"/>
+        <SiRaspberrypi className="moving-icon"/>
+        <SiJirasoftware className="moving-icon"/>
+      </div>
       </div>
     </div>
   );
