21c71267a3a289b8d369ff4b682039b38f5e71fd
rileycleavenger <riley.cleavenger@ufl.edu>
2024-02-22 14:48:30 -0500

    about me card done

--- riley-portfolio/src/components/about/about.tsx
+++ riley-portfolio/src/components/about/about.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import './about.css';
+import { FaLinkedin, FaGithub } from 'react-icons/fa';
 
 const About: React.FC = () => {
   return (
-    <div>
-      <h1>Welcome to the Home Page</h1>
-      <p>This is the home page of our React application.</p>
+    <div className="about">
+      <div className="container">
+        <div className="picture-and-name">
+          <img src={require('../../assets/headshot.png')} className="headshot" alt="Riley" />
+          <p className="name">Riley Cleavenger</p>
+        </div>
+        <div className="bio">
+          <h1>About Me</h1>
+          <p>I'm currently a 4th-year Computer Engineering major at the University of Florida looking to pursue a career in Software Engineering. Personally I enjoy working on full stack web development projects, but my enthusiasm extends to the realms of machine learning, artificial intelligence, and embedded systems, where I am keen on exploring innovative solutions and pushing the boundaries of technology.</p>
+          <div className="socials">
+            <a className="social-icon" href="https://www.linkedin.com/in/rileycleavenger/" target="_blank">
+              <FaLinkedin />
+            </a>
+            <a className="social-icon" href="https://github.com/rileycleavenger" target="_blank">
+              <FaGithub/>
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
