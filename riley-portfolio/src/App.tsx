965ae02ccd135f879e4b2ffc8f5691a37d60fda4
rileycleavenger <riley.cleavenger@ufl.edu>
2024-02-22 13:49:43 -0500

    finished navbar

--- riley-portfolio/src/App.tsx
+++ riley-portfolio/src/App.tsx
@@ -6,18 +6,22 @@ import SwProjects from './components/swProjects/swProjects';
 import HwProjects from './components/hwProjects/hwProjects';
 import Resume from './components/resume/resume';
 import ContactMe from './components/contactMe/contactMe';
+import './App.css';
+import './variables.css';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/software" element={<SwProjects />} />
-        <Route path="/hardware" element={<HwProjects />} />
-        <Route path="/resume" element={<Resume />} />
-        <Route path="/Contact" element={<ContactMe />} />
-      </Routes>
+      <div className="content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/software" element={<SwProjects />} />
+          <Route path="/hardware" element={<HwProjects />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="/Contact" element={<ContactMe />} />
+        </Routes>
+      </div>
     </Router>
   );
 };
--- riley-portfolio/src/components/navbar/navbar.tsx
+++ riley-portfolio/src/components/navbar/navbar.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './navbar.css';
+import '../../variables.css';
 
 const Navbar: React.FC = () => {
   return (
     <nav>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/hw_projects">Hardware Projects</Link></li>
-        <li><Link to="/sw_projects">Software Projects</Link></li>
-        <li><Link to="/resume">Resume</Link></li>
-        <li><Link to="/contact">Contact Me</Link></li>
-      </ul>
+        <Link to="/" className='navLink'>About Me</Link>
+        <Link to="/hardware" className='navLink'>Hardware Projects</Link>
+        <Link to="/software" className='navLink'>Software Projects</Link>
+        <Link to="/resume" className='navLink'>Resume</Link>
+        <Link to="/contact" className='navLink'>Contact Me</Link>
     </nav>
   );
 };
