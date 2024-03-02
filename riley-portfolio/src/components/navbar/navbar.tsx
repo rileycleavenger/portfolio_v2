bf1e622ae266d18e7cc6a5667b446c77745ad42d
rileycleavenger <riley.cleavenger@ufl.edu>
2024-03-02 18:31:33 -0500

    mobile navbar

--- riley-portfolio/src/components/navbar/navbar.tsx
+++ riley-portfolio/src/components/navbar/navbar.tsx
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './navbar.css';
 import '../../variables.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
 
 const Navbar: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDropdown = () => setIsOpen(!isOpen);
+
   return (
     <nav>
-        <Link to="/" className='navLink'>About Me</Link>
-        <Link to="/software" className='navLink'>Software Projects</Link>
-        <Link to="/hardware" className='navLink'>Hardware Projects</Link>
-        <Link to="/resume" className='navLink'>Resume</Link>
+      <Link to="/" className='navLink'>About Me</Link>
+      <Link to="/software" className='navLink'>Software Projects</Link>
+      <Link to="/hardware" className='navLink'>Hardware Projects</Link>
+      <Link to="/resume" className='navLink'>Resume</Link>
+      <div className='dropdownMenu'>
+        <FontAwesomeIcon className="menu-button" icon={faBars} onClick={toggleDropdown}/>
+        {isOpen && (
+        <div className='dropdownContent'>
+          <Link to="/" className='menuLink'>About Me</Link>
+          <Link to="/software" className='menuLink'>Software Projects</Link>
+          <Link to="/hardware" className='menuLink'>Hardware Projects</Link>
+          <Link to="/resume" className='menuLink'>Resume</Link>
+        </div>
+        )}
+      </div>
     </nav>
   );
 };
