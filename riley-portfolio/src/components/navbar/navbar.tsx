23d0a0ab3957a12c3b5efff0a95e92a6ccbd2647
rileycleavenger <riley.cleavenger@ufl.edu>
2024-03-02 18:58:56 -0500

    finally done

--- riley-portfolio/src/components/navbar/navbar.tsx
+++ riley-portfolio/src/components/navbar/navbar.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './navbar.css';
 import '../../variables.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -11,14 +11,29 @@ const Navbar: React.FC = () => {
 
   const toggleDropdown = () => setIsOpen(!isOpen);
 
+  const location = useLocation();
+
+  function convertPathToTitle(path: string) {
+    if (path === '/') {
+      return 'About Me';
+    } else if (path === '/software') {
+      return 'Software Projects';
+    } else if (path === '/hardware') {
+      return 'Hardware Projects';
+    } else if (path === '/resume') {
+      return 'Resume';
+    }
+  }
+
   return (
     <nav>
-      <Link to="/" className='navLink'>About Me</Link>
-      <Link to="/software" className='navLink'>Software Projects</Link>
-      <Link to="/hardware" className='navLink'>Hardware Projects</Link>
-      <Link to="/resume" className='navLink'>Resume</Link>
+      <Link to="/" className={location.pathname === '/' ? 'navLink active' : 'navLink'}>About Me</Link>
+      <Link to="/software" className={location.pathname === '/software' ? 'navLink active' : 'navLink'}>Software Projects</Link>
+      <Link to="/hardware" className={location.pathname === '/hardware' ? 'navLink active' : 'navLink'}>Hardware Projects</Link>
+      <Link to="/resume" className={location.pathname === '/resume' ? 'navLink active' : 'navLink'}>Resume</Link>
       <div className='dropdownMenu'>
         <FontAwesomeIcon className="menu-button" icon={faBars} onClick={toggleDropdown}/>
+        <p className="page-title">{convertPathToTitle(location.pathname)}</p>
         {isOpen && (
         <div className='dropdownContent'>
           <Link to="/" onClick={toggleDropdown} className='menuLink'>About Me</Link>
